aa7c8a6a4d8da4994b4ad2c710a483b38a2e24e3
youhaibo1909 <youhaibo1990@163.com>
2018-10-05 17:51:14 +0800

    blog使用index、post html文件渲染

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template.loader import get_template
+from datetime import datetime
+
 from .models import Post
+
 # Create your views here.
 import sys
 
@@ -11,11 +16,28 @@ import sys
 #     reload(sys)
 #     sys.setdefaultencoding('utf-8')
 
+# def homepage(request):
+#     posts = Post.objects.all()
+#     post_lists = list()
+#     for count, post in enumerate(posts):
+#         print (count, post, type(post.body))
+#         post_lists.append("No.{}:".format(str(count).decode('utf-8')) + str(post).decode('utf-8')+"<hr>")
+#         post_lists.append("<small>" + str(post.body.encode('utf-8')).decode('utf-8') + "</small><br><br>")
+#     return HttpResponse(post_lists)
+
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        print (count, post, type(post.body))
-        post_lists.append("No.{}:".format(str(count).decode('utf-8')) + str(post).decode('utf-8')+"<hr>")
-        post_lists.append("<small>" + str(post.body.encode('utf-8')).decode('utf-8') + "</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
